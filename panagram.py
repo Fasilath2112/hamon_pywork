873c12c1002b81d879f695f80f04a876ccbd7681
Fasilath2112 <151925141+Fasilath2112@users.noreply.github.com>
2024-05-21 17:22:06 +0530

    Update panagram.py

--- panagram.py
+++ panagram.py
@@ -1,11 +1,11 @@
 def is_panagram(s):
     s = s.lower()
-    a = set('abcdefghijklmnopqrstuvwxyz')
-    for char in a:
+    alphabets = set('abcdefghijklmnopqrstuvwxyz')
+    for char in alphabets:
         if char not in s:
             return False
     return True
 
 if __name__ == '__main__':
-    str1 = "the quick brown fox jumps over the lazy dog"
-    print(is_panagram(str1))
+    string = "the quick brown fox jumps over the lazy dog"
+    print(is_panagram(string))
